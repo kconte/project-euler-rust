df35aa4207a2622d12ba0f70f91fe2928da5d3fc
Kevin Conte <kevin@conte.net>
2021-09-11 18:38:32 -0700

    added runner harness for single problems

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -4,40 +4,124 @@
 mod lib;
 
 use clap::{App, Arg, SubCommand};
+use lib::{SolverResult, TimeFormatter};
+use std::{path::Path, process::Command};
 
 fn main() {
   // Determine what actions will be taken
-  let _matches = App::new("Project Euler")
+  let matches = App::new("Project Euler")
     .version("0.1.0")
     .author("Kevin Conte <kevin@conte.net>")
     .about("Runner and Benchmarker for Project Euler solutions")
+    .arg(
+      Arg::with_name("color")
+        .long("color")
+        .help("Print using colors"),
+    )
+    .arg(
+      Arg::with_name("root")
+        .long("root")
+        .short("r")
+        .help("Root directory from which to run solvers")
+        .default_value("./solvers"),
+    )
     .subcommand(
       SubCommand::with_name("run")
-        .about("runs a particular problem")
+        .about("Runs a particular problem")
         .version("0.1.0")
         .author("Kevin Conte<kevin@conte.net>")
         .arg(
           Arg::with_name("all")
             .long("all")
             .short("a")
-            .help("run all problem solvers")
+            .help("Run all problem solvers")
             .conflicts_with("numbers"),
         )
         .arg(Arg::with_name("numbers").multiple(true)),
     )
     .subcommand(
       SubCommand::with_name("benchmark")
-        .about("benchmarks a particular problem")
+        .about("Benchmarks a particular problem")
         .version("0.1.0")
         .author("Kevin Conte<kevin@conte.net>")
         .arg(
           Arg::with_name("all")
             .long("all")
             .short("a")
-            .help("benchmark all problem solvers")
+            .help("Benchmark all problem solvers")
             .conflicts_with("numbers"),
+        )
+        .arg(
+          Arg::with_name("iterations")
+            .long("iterations")
+            .short("i")
+            .help("Number of samples to take")
+            .default_value("1000"),
         )
         .arg(Arg::with_name("numbers").multiple(true)),
     )
     .get_matches();
+
+  let root = Path::new(matches.value_of("root").unwrap());
+  let use_color = matches.is_present("color");
+
+  if let Some(matches) = matches.subcommand_matches("run") {
+    let mut indexes = matches.values_of("numbers").unwrap_or_default().peekable();
+
+    if !matches.is_present("all") && indexes.peek().is_none() {}
+
+    if matches.is_present("all") {
+      // TODO: iterate all implemented solvers and run each of them
+      unimplemented!()
+    } else if indexes.peek().is_some() {
+      for index in indexes {
+        let name = format!("{:03}", index.parse::<u64>().unwrap());
+        print!("Running {} . . .", name);
+        let path = root.join(name);
+        let res = run_one(path);
+        if use_color {
+          println!(
+            "\r{}{} {}{}{}                               ",
+            termion::color::Fg(termion::color::Green),
+            res.result,
+            termion::color::Fg(termion::color::Cyan),
+            res.time_taken.format_as_time(),
+            termion::color::Fg(termion::color::Reset),
+          );
+        } else {
+          println!("\r{}                                 ", res);
+        }
+      }
+    } else {
+      eprintln!("Must provide at least one solver to run.");
+      std::process::exit(1);
+    }
+  }
+}
+
+fn run_one<P: AsRef<Path>>(idx: P) -> SolverResult {
+  let mut cmd = Command::new("cargo");
+  cmd.arg("run");
+  cmd.arg("--quiet");
+  cmd.arg("--release");
+  cmd.current_dir(idx.as_ref());
+
+  SolverResult::from(cmd.output().unwrap().stdout)
+}
+
+/// Debug utility that prints the type of a value.
+///
+/// # Examples
+/// Basic usage:
+/// ```rust
+/// let a: u64 = 100;
+/// print_type(&a);
+/// // u64
+///
+/// let b: Vec<u8> = vec![]
+/// print_type(&b);
+/// // alloc::vec::Vec<u8>
+/// ```
+pub fn print_type<T>(_: &T) {
+  println!("{}", std::any::type_name::<T>());
 }
