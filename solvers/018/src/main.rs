6c51b4e7c23ac933885352b9ffb69b1686bfbfc7
Kevin Conte <kevin@conte.net>
2021-09-12 15:02:14 -0700

    018

---
+++ solvers/018/src/main.rs
@@ -0,0 +1,51 @@
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+
+fn main() {
+  run();
+}
+
+fn run() {
+  let start = std::time::Instant::now();
+
+  // code goes here
+
+  let mut triangle: Vec<Vec<u64>> = TRIANGLE
+    .trim()
+    .lines()
+    .map(|l| {
+      l.split_ascii_whitespace()
+        .map(|v| v.parse::<u64>().expect("could not parse u64"))
+        .collect()
+    })
+    .collect();
+
+  for i in (0..triangle.len() - 1).rev() {
+    for j in 0..triangle[i].len() {
+      triangle[i][j] += std::cmp::max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+    }
+  }
+
+  let res = triangle[0][0];
+
+  let span = start.elapsed().as_nanos();
+  println!("{} {}", res, span);
+}
+
+const TRIANGLE: &str = "
+  75
+  95 64
+  17 47 82
+  18 35 87 10
+  20 04 82 47 65
+  19 01 23 75 03 34
+  88 02 77 73 07 63 67
+  99 65 04 28 06 16 70 92
+  41 41 26 56 83 40 80 70 33
+  41 48 72 33 47 32 37 16 94 29
+  53 71 44 65 25 43 91 52 97 51 14
+  70 11 33 28 77 73 17 78 39 68 17 57
+  91 71 52 38 17 14 91 43 58 50 27 29 48
+  63 66 04 68 89 53 67 30 73 16 69 87 40 31
+  04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+";
