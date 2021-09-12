cf5160d8e5ded7cf2c495e96aa001fcf65f3d600
Kevin Conte <kevin@conte.net>
2021-09-11 20:06:43 -0700

    formatting update

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,15 +1,45 @@
 #![warn(clippy::all)]
 #![warn(clippy::pedantic)]
+#![allow(clippy::non_ascii_literal)]
 
 mod lib;
 
 use clap::{App, Arg, SubCommand};
 use lib::{SolverResult, TimeFormatter};
+use pad::{Alignment, PadStr};
 use std::{path::Path, process::Command};
 
 fn main() {
-  // Determine what actions will be taken
-  let matches = App::new("Project Euler")
+  let matches = get_matches();
+
+  let root = Path::new(matches.value_of("root").unwrap());
+  let use_color = !matches.is_present("no-color");
+
+  if let Some(matches) = matches.subcommand_matches("run") {
+    let mut indexes = matches.values_of("numbers").unwrap_or_default().peekable();
+
+    if matches.is_present("all") {
+      print_header();
+      // TODO: iterate all implemented solvers and run each of them
+      print_footer();
+    } else if indexes.peek().is_some() {
+      print_header();
+      for index in indexes {
+        let name = format!("{:03}", index.parse::<u64>().unwrap());
+        let path = root.join(name);
+        let res = run_one(path);
+        print_record(index.parse::<u64>().unwrap(), res, use_color);
+      }
+      print_footer();
+    } else {
+      eprintln!("Must provide at least one solver to run.");
+      std::process::exit(1);
+    }
+  }
+}
+
+fn get_matches() -> clap::ArgMatches<'static> {
+  App::new("Project Euler")
     .version("0.1.0")
     .author("Kevin Conte <kevin@conte.net>")
     .about("Runner and Benchmarker for Project Euler solutions")
@@ -60,55 +90,7 @@ fn main() {
         )
         .arg(Arg::with_name("numbers").multiple(true)),
     )
-    .get_matches();
-
-  let root = Path::new(matches.value_of("root").unwrap());
-  let use_color = !matches.is_present("no-color");
-
-  if let Some(matches) = matches.subcommand_matches("run") {
-    let mut indexes = matches.values_of("numbers").unwrap_or_default().peekable();
-
-    if !matches.is_present("all") && indexes.peek().is_none() {}
-
-    if matches.is_present("all") {
-      // TODO: iterate all implemented solvers and run each of them
-      unimplemented!()
-    } else if indexes.peek().is_some() {
-      println!(
-        "\n{}Project Euler{}",
-        termion::style::Bold,
-        termion::style::Reset
-      );
-      println!("-------------");
-      for index in indexes {
-        let name = format!("{:03}", index.parse::<u64>().unwrap());
-        print!("Running {} . . .", name);
-        let path = root.join(name);
-        let res = run_one(path);
-        if use_color {
-          println!(
-            "\r{}{:-3}    {}{}    {}{}{}                               ",
-            termion::color::Fg(termion::color::Magenta),
-            index.parse::<u64>().unwrap(),
-            termion::color::Fg(termion::color::Green),
-            res.result,
-            termion::color::Fg(termion::color::Cyan),
-            res.time_taken.format_as_time(),
-            termion::color::Fg(termion::color::Reset),
-          );
-        } else {
-          println!(
-            "\r{}    {}                                 ",
-            index.parse::<u64>().unwrap(),
-            res
-          );
-        }
-      }
-    } else {
-      eprintln!("Must provide at least one solver to run.");
-      std::process::exit(1);
-    }
-  }
+    .get_matches()
 }
 
 fn run_one<P: AsRef<Path>>(idx: P) -> SolverResult {
@@ -136,4 +118,48 @@ fn run_one<P: AsRef<Path>>(idx: P) -> SolverResult {
 /// ```
 pub fn print_type<T>(_: &T) {
   println!("{}", std::any::type_name::<T>());
+}
+
+fn print_header() {
+  println!("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+  println!(
+    "{}┃              Project Euler              ┃{}",
+    termion::style::Bold,
+    termion::style::Reset
+  );
+  println!("┣━━━━━┳━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━┫");
+  println!(
+    "{}┃ idx ┃      result     ┃     runtime     ┃{}",
+    termion::style::Bold,
+    termion::style::Reset
+  );
+  println!("┡━━━━━╇━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━┩");
+}
+
+fn print_footer() {
+  println!("└─────┴─────────────────┴─────────────────┘");
+}
+
+fn print_record(idx: u64, res: SolverResult, use_color: bool) {
+  let idx_str = idx
+    .to_string()
+    .pad_to_width_with_alignment(3, Alignment::Right);
+  let res_str = res.result.to_string().pad_to_width(15);
+  let tim_str = res.time_taken.format_as_time().pad_to_width(15);
+
+  if use_color {
+    println!(
+      "│ {}{}{} │ {}{}{} │ {}{}{} │",
+      termion::color::Fg(termion::color::Magenta),
+      idx_str,
+      termion::color::Fg(termion::color::Reset),
+      termion::color::Fg(termion::color::Green),
+      res_str,
+      termion::color::Fg(termion::color::Reset),
+      termion::color::Fg(termion::color::Cyan),
+      tim_str,
+      termion::color::Fg(termion::color::Reset),
+    );
+  } else {
+  }
 }
